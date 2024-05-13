c5a734ad3c4ecda63d7a3d8d7beae26836b098e2
pereag <perello.valentin@hotmail.fr>
2024-05-13 17:19:30 +0200

    feat: add store

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,18 +1,21 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
+import { AccountsService } from '../account/accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService, AccountsService],
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  constructor(
+    private loggingService: LoggingService,
+    private accountServices: AccountsService
+  ) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.accountServices.addAccount(accountName, accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
